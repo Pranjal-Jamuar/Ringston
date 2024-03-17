2936ee1696b739de5394bd0435147f085dc0ff92
Pranjal Jamuar <pranjal.jamuar@gmail.com>
2024-03-17 17:16:17 +0530

    Inspection Section done

--- main.js
+++ main.js
@@ -137,6 +137,39 @@ function animateWords() {
   setInterval(updateText, 3000)
 }
 
+const inspectParallax = () => {
+  const inspectionTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".inspection",
+      start: "top bottom",
+      end: "bottom top",
+      scrub: true,
+    },
+  })
+  inspectionTl
+    .to(".inspection h2", {
+      y: -100,
+    })
+    .to(
+      ".ring-bg",
+      {
+        y: -50,
+        height: 300,
+      },
+      "<"
+    )
+
+  gsap.to(".marquee h3", {
+    scrollTrigger: {
+      trigger: ".marquee h3",
+      start: "top 80%",
+      end: "bottom top",
+      scrub: true,
+    },
+    x: 200,
+  })
+}
+
 function initRenderLoop() {
   const clock = new THREE.Clock()
 
@@ -183,5 +216,6 @@ document.addEventListener("DOMContentLoaded", () => {
   initRenderLoop()
 
   animateWords()
+  inspectParallax()
   smoothScrollSetup()
 })
